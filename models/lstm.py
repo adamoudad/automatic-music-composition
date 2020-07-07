29bb5e2971e2dd8db30624689b2c58bb23b852a6
adamoudad <adam.oudad@gmail.com>
2020-07-08 02:29:01 +0900

    Add dropout parameter and clean unused imports

--- models/lstm.py
+++ models/lstm.py
@@ -1,8 +1,9 @@
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, TimeDistributed, Cropping1D
-from keras.layers import LSTM, Lambda, Embedding
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import LSTM, Lambda
 
-def two_layers_lstm(sequence_length, input_size, n_classes):
+
+def two_layers_lstm(sequence_length, input_size, n_classes, dropout=0.3):
     '''
     Baseline model. Two layers LSTM.
     <https://towardsdatascience.com/how-to-generate-music-using-a-lstm-neural-network-in-keras-68786834d4c5>
@@ -13,12 +14,12 @@ def two_layers_lstm(sequence_length, input_size, n_classes):
         256,
         return_sequences=True
     ))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout))
     model.add(LSTM(512, return_sequences=True))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout))
     model.add(LSTM(256))
     model.add(Dense(256))
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout))
     model.add(Dense(n_classes))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
